288b2f3ecb15f801fd41caf14d9916d202934181
Darío Fernando Gallegos Quishpe <dfgallegos@eservicios.indra.es>
2022-06-30 10:47:04 +0200

    Basic structure of Xcode project

---
+++ ReaderRss/ReaderRss/ReaderRssApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderRssApp.swift
+//  ReaderRss
+//
+//  Created by Darío Gallegos on 30/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ReaderRssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
